430102699c890ed40cd4136f2ec1f7c1f52613f9
Space-Belt <dngurwn@gmail.com>
2025-10-27 23:34:12 +0900

    [FEAT] SecureStore 로그인 훅 추가

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,61 +1,59 @@
+import { ISecureStoreAuthData } from "@/types/auth";
 import * as SecureStore from "expo-secure-store";
 import { useCallback, useEffect, useState } from "react";
 
-const TOKEN_KEY = "auth_token";
+const KEY = "auth_data";
 
 export const useAuth = () => {
-  const [token, setToken] = useState<string | null>(null);
+  const [auth, setAuth] = useState<ISecureStoreAuthData | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // 토큰 불러오기 (초기화)
   useEffect(() => {
-    loadToken();
+    loadAuth();
   }, []);
 
-  const loadToken = async () => {
+  const loadAuth = async () => {
     try {
-      const storedToken = await SecureStore.getItemAsync(TOKEN_KEY);
-      setToken(storedToken);
-      setIsAuthenticated(!!storedToken);
-    } catch (error) {
-      console.error("토큰 불러오기 실패:", error);
+      const data = await SecureStore.getItemAsync(KEY);
+      if (data) {
+        const parsed: ISecureStoreAuthData = JSON.parse(data);
+        setAuth(parsed);
+        setIsAuthenticated(true);
+      }
+    } catch (e) {
+      console.error("auth 불러오기 실패:", e);
     } finally {
       setIsLoading(false);
     }
   };
 
-  // 로그인 (토큰 저장)
-  const login = useCallback(async (newToken: string) => {
+  // 로그인
+  const login = useCallback(async (data: ISecureStoreAuthData) => {
     try {
-      await SecureStore.setItemAsync(TOKEN_KEY, newToken);
-      setToken(newToken);
+      await SecureStore.setItemAsync(KEY, JSON.stringify(data));
+      setAuth(data);
       setIsAuthenticated(true);
       return true;
-    } catch (error) {
-      console.error("로그인 실패:", error);
+    } catch (e) {
+      console.error("로그인 실패:", e);
       return false;
     }
   }, []);
 
-  // 로그아웃 (토큰 삭제)
+  // 로그아웃
   const logout = useCallback(async () => {
     try {
-      await SecureStore.deleteItemAsync(TOKEN_KEY);
-      setToken(null);
+      await SecureStore.deleteItemAsync(KEY);
+      setAuth(null);
       setIsAuthenticated(false);
+      console.log("로그아웃 성공 !!");
       return true;
-    } catch (error) {
-      console.error("로그아웃 실패:", error);
+    } catch (e) {
+      console.error("로그아웃 실패:", e);
       return false;
     }
   }, []);
 
-  return {
-    token,
-    isAuthenticated,
-    isLoading,
-    login,
-    logout,
-  };
+  return { auth, isAuthenticated, isLoading, login, logout };
 };
