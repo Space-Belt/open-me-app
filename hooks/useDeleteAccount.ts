3d13cc534b34132038c31a43577c5cf3cb319420
Space-Belt <dngurwn@gmail.com>
2025-10-30 15:08:28 +0900

    [FEAT] 회원 탈퇴 훅 추가

---
+++ hooks/useDeleteAccount.ts
@@ -0,0 +1,21 @@
+import { deleteAccount } from "@/api/authController";
+import { useAuth } from "@/hooks/useAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
+
+export const useDeleteAccount = () => {
+  const queryClient = useQueryClient();
+  const { logout } = useAuth();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: deleteAccount,
+    onSuccess: async (result) => {
+      if (result.success) {
+        queryClient.invalidateQueries({ queryKey: ["posts"] });
+        await logout();
+        router.replace("/(auth)/signin");
+      }
+    },
+  });
+};
