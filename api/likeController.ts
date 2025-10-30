1ff9a90a6f6b36d23d6afe37e446e1f9e6b31426
Space-Belt <dngurwn@gmail.com>
2025-10-30 15:14:47 +0900

    [FEAT] 좋아요 컨트롤러 추가

---
+++ api/likeController.ts
@@ -0,0 +1,36 @@
+import { db } from "@/lib/firebase";
+import {
+  deleteDoc,
+  doc,
+  getDoc,
+  increment,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+
+// 좋아요 등록
+export const likePost = async (postId: string, userId: string) => {
+  const likeRef = doc(db, "posts", postId, "likes", userId);
+  // 1. likes 서브컬렉션에 내 userId로 도큐먼트 생성
+  await setDoc(likeRef, { createdAt: Date.now() });
+
+  // 2. 게시글 likeCount +1
+  const postRef = doc(db, "posts", postId);
+  await updateDoc(postRef, { likeCount: increment(1) });
+};
+
+// 좋아요 취소
+export const unlikePost = async (postId: string, userId: string) => {
+  const likeRef = doc(db, "posts", postId, "likes", userId);
+  await deleteDoc(likeRef);
+
+  const postRef = doc(db, "posts", postId);
+  await updateDoc(postRef, { likeCount: increment(-1) });
+};
+
+// 내가 좋아요했는지
+export const checkPostLiked = async (postId: string, userId: string) => {
+  const likeRef = doc(db, "posts", postId, "likes", userId);
+  const likeSnap = await getDoc(likeRef);
+  return likeSnap.exists();
+};
