c286a85478c84e899b0cda8e2dd7d33aa8e972e8
Space-Belt <dngurwn@gmail.com>
2025-10-30 15:09:45 +0900

    [FEAT] 글 올리기 mutation 훅 수정하기 추가

--- hooks/usePostMutation.ts
+++ hooks/usePostMutation.ts
@@ -1,36 +1,43 @@
 import { uploadPostImages } from "@/api/imageController";
-import { createPost } from "@/api/postController";
-import { IPost } from "@/types/post";
-import { useMutation } from "@tanstack/react-query";
+import {
+  createPost,
+  deletePostWithComments,
+  updatePost,
+} from "@/api/postController";
+import { IGetPostedData } from "@/types/post";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const usePostMutation = (
   successCallBack: (isSuccess: boolean) => void,
-  post?: IPost,
+  post?: IGetPostedData,
   uid?: string,
   displayName?: string,
   photoURL?: string
 ) => {
-  const mutation = useMutation({
+  const queryClient = useQueryClient();
+  const createPostUpdatemutation = useMutation({
     mutationFn: async ({
       title,
       content,
       images,
+      postId,
     }: {
       title: string;
       content: string;
       images: string[];
+      postId?: string;
     }) => {
-      console.log("게시물 등록 시작");
-      console.log(images);
-
       const imageUrls =
         images.length > 0 ? await uploadPostImages(images, uid!) : [];
 
-      console.log("이미지 등록 성공", imageUrls);
-
       // 수정(이미 존재 post.id 있음) / 등록 분기
-      if (post && post.id) {
-        return;
+      if (post && postId) {
+        const updatedPostId = await updatePost(postId, {
+          title,
+          content,
+          imageUrls,
+        });
+        return updatedPostId;
       } else {
         const postId = await createPost({
           title,
@@ -40,18 +47,33 @@ const usePostMutation = (
           displayName: displayName || "",
           photoURL,
         });
-        console.log("게시물 등록 성공", imageUrls);
+
         return postId;
       }
     },
     onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+
       successCallBack(true);
     },
     onError: (error) => {
       successCallBack(false);
     },
   });
-  return { mutation };
+
+  const deleteMutation = useMutation({
+    mutationFn: async (postIdToDelete: string) => {
+      await deletePostWithComments(postIdToDelete);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      successCallBack(true);
+    },
+    onError: () => {
+      successCallBack(false);
+    },
+  });
+  return { createPostUpdatemutation, deleteMutation };
 };
 
 export default usePostMutation;
