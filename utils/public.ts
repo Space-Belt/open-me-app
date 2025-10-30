da8cf95d424ae1ce800f1702d216d872cfe02054
Space-Belt <dngurwn@gmail.com>
2025-10-30 14:57:49 +0900

    chore : 공통 사용 페이지 불러오기 추가

--- utils/public.ts
+++ utils/public.ts
@@ -2,5 +2,7 @@ import { Dimensions, Platform } from "react-native";
 
 export const isIOS = Platform.OS === "ios";
 
+export const POSTS_PAGE_SIZE = 10;
+
 export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
   Dimensions.get("window");
